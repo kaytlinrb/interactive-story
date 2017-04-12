166bba0c94cc755f6f4ef9268c58bdfc291b5ae3
Jon Kang <Guest@epicodus-5.local>
2017-04-12 11:28:50 -0700

    finished combat, pig

---
+++ js/combat.js
@@ -0,0 +1,179 @@
+function coinToss(){
+  return Math.ceil(Math.random()*2);
+}
+var queryStats = window.location.hash.substring(1);
+var playerTurn = true;
+//initial stats
+var origPlayer = {name: 'Player', id: 'You', health: 20, damage: 3, will: 2, defense: 2};
+// var origEnemy = {name: 'Enemy', id: 'enemy', health: queryStats.substring(0,2), damage: queryStats.substring(2,3), will: queryStats.substring(3,4), defense:queryStats.substring(4,5)}
+var origEnemy = {name: 'Enemy', id: 'enemy', health: 20, damage: 3, will: 3, defense:2}
+//modified stats
+var playerTest = {name: 'Player', id: 'You',  health: 20,damage: 3, will: 2, defense: 2};
+var enemyTest = {name: 'Enemy', id: 'enemy', health: 20, damage: 3, will: 3, defense:2}
+
+//turn switch
+function turnSwap(){
+  playerTurn=!playerTurn;
+  $('#YouName').toggleClass('activePlayer');
+  $('#enemyName').toggleClass('activePlayer');
+}
+
+//Attack function, roll 1-6 and add damage stat, subtract target defense
+function damage(attacker, defender){
+  var roll = Math.ceil(Math.random()*6);
+  var damage = Math.abs((roll+attacker.damage)-defender.defense)
+  if(roll===6){
+    attack *= 2;
+  }
+  $('#'+attacker.id+'Log').append("<li>"+damage+" damage dealt.</li>")
+  defender.health -= damage;
+  $('#'+defender.id+'Health').text('Health: '+defender.health)
+  if(playerTest.health<=0){
+    $('#enemyLog').append('<li>You are slain!</li>');
+    $('.btn').hide();
+    $('#proceedLose').show();
+  }
+  else{
+    turnSwap();
+  }
+}
+
+//Charge function, roll 1-6, add damage. Ignore armor but suffer 1/3 of damage.
+function charge(attacker, defender){
+  var roll = Math.ceil(Math.random()*6);
+  var damage = Math.abs(roll+attacker.damage)
+  attacker.health -= Math.round(damage/3);
+  if(roll===6){
+    attack *= 2;
+  }
+  $('#'+attacker.id+'Log').append("<li>"+damage+" charge damage dealt. "+Math.round(damage/3)+" recoil damage taken.</li>")
+  defender.health -= damage;
+  $('#'+attacker.id+'Health').text('Health: '+attacker.health)
+  $('#'+defender.id+'Health').text('Health: '+defender.health)
+  if(playerTest.health<=0){
+    $('#enemyLog').append('<li>You are slain!</li>');
+    $('.btn').hide();
+    $('#proceedLose').show();
+  }
+  else{
+  turnSwap();
+  }
+}
+
+//Defend function, increase defense by 1 to a maximum of +6
+function defend(defender, origDefender){
+  if(defender.defense+1<=origDefender.defense+6){
+    defender.defense += 1;
+    $('#'+defender.id+'Log').append("<li> Defense rises to "+defender.defense+".</li>")
+  }
+  else{
+    $('#'+defender.id+'Log').append('<li>Defense is at maximum</li>')
+  }
+  $('#'+defender.id+'Defense').text('Defense: '+defender.defense)
+  turnSwap();
+}
+
+//Heal function, heal for 1d6 + will
+function heal(defender, origDefender){
+  var roll = Math.ceil(Math.random()*4);
+  var heal = roll+defender.will;
+  console.log(heal,roll, defender.will);
+  if(defender.health+heal<=origDefender.health){
+    defender.health += heal;
+  }
+  else{
+    defender.health = origDefender.health;
+  }
+  $('#'+defender.id+'Log').append("<li>"+heal+' health healed.</li>')
+  $('#'+defender.id+'Health').text('Health: '+defender.health)
+  turnSwap();
+}
+
+//AI script, if high health, will charge, attack or defend. On low health will either attack or heal.
+function aiTurn(){
+  var decision = Math.ceil(Math.random()*6);
+  if(enemyTest.health>=origEnemy.health*.4){//above 40% health
+    if(playerTest.defense>origPlayer.defense+2){
+      charge(enemyTest, playerTest);
+    }
+    else if(decision>2){
+      damage(enemyTest, playerTest);
+    }
+    else{
+      defend(enemyTest, origEnemy);
+    }
+  }
+  else{//below 40% health
+    if(decision>2){
+      heal(enemyTest, origEnemy);
+    }
+    else{
+      damage(enemyTest, playerTest);
+    }
+  }
+}
+
+$(document).ready(function(){
+  $('#YouHealth').text('Health: '+origPlayer.health);
+  $('#YouDefense').text('Defense: '+origPlayer.defense);
+  $('#YouName').text(origPlayer.name);
+  $('#enemyHealth').text('Health: '+origEnemy.health);
+  $('#enemyDefense').text('Defense: '+origEnemy.defense);
+  $('#enemyName').text(origEnemy.name);
+
+  //Chooses first player at random
+  if(coinToss()===1){
+    playerTurn = false;
+    $('#enemyName').addClass('activePlayer');
+    setTimeout(function(){alert('The enemy strikes first!');aiTurn();}, 1000);
+  }
+  else{
+    $('#YouName').addClass('activePlayer');
+  }
+
+  //click functions for player combat
+  $('#attack').click(function(){
+    if(playerTurn){
+      damage(playerTest, enemyTest);
+      if(enemyTest.health<=0){
+        $('#YouLog').append('<li>The enemy is slain!</li>');
+        $('.btn').hide();
+        $('#proceedWin').show();
+      }
+      else{
+        setTimeout(function(){aiTurn();}, 2000);
+      }
+    }
+  });
+  $('#charge').click(function(){
+    if(playerTurn){
+      charge(playerTest, enemyTest);
+      if(enemyTest.health<=0){
+        $('#YouLog').append('<li>The enemy is slain!</li>');
+        $('.btn').hide();
+        $('#proceedWin').show();
+      }
+      else{
+        setTimeout(function(){aiTurn();}, 2000);
+      }
+    }
+  });
+  $('#defend').click(function(){
+    if(playerTurn){
+      defend(playerTest, origPlayer);
+      setTimeout(function(){aiTurn();}, 2000);
+    }
+  });
+  $('#heal').click(function(){
+    if(playerTurn){
+      heal(playerTest, origPlayer);
+      setTimeout(function(){aiTurn();}, 1500);
+    }
+  });
+  $('#proceedWin').click(function(){
+    alert('win');
+  });
+  $('#proceedLose').click(function(){
+    alert('lost');
+  });
+});
---
+++ js/pig.js
@@ -0,0 +1,139 @@
+$(document).ready(function(){
+  var turnTotal = 0;
+  var activeTotal = 0;
+  var inactiveTotal = 0;
+  //coin flip to determine first player
+  var activePlayer = "";
+  function coinFlip(){
+    var flip = 2;
+    // var flip = Math.ceil(Math.random() * 2);
+    if (flip===1){
+      activePlayer='player1';
+    }
+    else if(flip===2&&cpuPlayer===true){
+      activePlayer='player2'
+      hit();
+    }
+    else{
+      activePlayer='player2'
+      alert(activePlayer+' is first');
+    }
+    $('#'+activePlayer+'-title').addClass('activePlayer');
+  }
+  //determines if player 2 is a human or cpu
+  var cpuPlayer = true;
+  coinFlip();
+
+  //resets the game on win or reset button
+  function reset(){
+    turnTotal = 0;
+    activeTotal = 0;
+    inactiveTotal = 0;
+    activePlayer = "";
+    $('#player1-history').empty();
+    $('#player2-history').empty();
+    $('#player1-total').empty();
+    $('#player2-total').empty();
+  }
+  //simulates d6 roll
+  function diceRoll() {
+    return Math.ceil(Math.random() * 6);
+  }
+
+  //script for players taking turns
+  function swap(){
+    //switch player totals
+    var third = activeTotal;
+    activeTotal = inactiveTotal;
+    inactiveTotal = third;
+    $('#'+activePlayer+'-title').removeClass('activePlayer');
+    //switch active player
+    if(activePlayer==='player1'&&cpuPlayer===true){
+      activePlayer = 'player2';
+      hit();
+    }
+    else if(activePlayer==='player1'){
+      activePlayer = 'player2';
+    }
+    else{
+      activePlayer = 'player1';
+    }
+    $('#'+activePlayer+'-title').addClass('activePlayer');
+  }
+
+  function hit(){
+    var roll = diceRoll();
+    var riskFactor = 20;
+
+    if(inactiveTotal-activeTotal>20){
+      riskFactor = 25;}
+    else if(activeTotal-inactiveTotal>20){
+      riskFactor = 10;
+    }
+
+    //expected value is good, hit
+    if(roll!=1){
+      turnTotal += roll;
+            console.log(activePlayer, cpuPlayer, turnTotal, riskFactor);
+      $('#'+activePlayer+'-history').append('<li>'+roll+'</li>');
+      if(activePlayer==='player2'&&cpuPlayer===true&&(turnTotal+activeTotal)>=50){
+        hold();
+      }
+      else if(activePlayer==='player2'&&cpuPlayer===true&&turnTotal<riskFactor){
+        setTimeout(function(){hit();},750);
+      }
+      else if(activePlayer==='player2'&&cpuPlayer===true){
+        hold();
+      }
+    }
+    //bust
+    else{
+      turnTotal = 0;
+      $('#'+activePlayer+'-history').append('<li>bust</li>');
+      $('#'+activePlayer+'-total').text(activeTotal);
+      swap();
+    }
+  }
+  function hold(){
+    activeTotal += turnTotal
+    if(activeTotal>=50){
+      $('.btn').hide();
+      if(activePlayer==='player1'){
+        $('#proceedWin').show();
+      }
+      else{
+        $('#proceedLose').show();
+      }
+      $('#'+activePlayer+'-history').append('<li>Win!</li>');
+    }
+    else{
+      turnTotal = 0;
+      $('#'+activePlayer+'-history').append('<li>hold</li>');
+      $('#'+activePlayer+'-total').text(activeTotal);
+      swap();
+    }
+  }
+
+  $('#hit').click(function() {
+    if(activePlayer==='player1'){
+      hit();
+    }
+  });
+  $('#hold').click(function(){
+    if(activePlayer==='player1'){
+      hold();
+    }
+  });
+  $('#reset').click(function() {
+    if(activePlayer==='player1'){
+      reset();
+    }
+  });
+  $('#proceedWin').click(function(){
+    alert('win');
+  });
+  $('#proceedLose').click(function(){
+    alert('lost');
+  });
+
+});
